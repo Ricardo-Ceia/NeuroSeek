8046a16e3dc4b34a5b9b2ebd634e41d4df664797
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-02-19 20:01:02 +0000

    vector core operations

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ class Vector:
         self.size = size
         self.data = [0] * size
 
+    def __getitem_(self,index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.size
+    
+    def __repr__(self):
+        return f"{self.data}"
+
 def main():
     print("Hello,World!");
     testVec = Vector(5)
-    print(testVec.data)
+    print(len(testVec))
 
 main()
