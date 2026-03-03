80f33653d2db53dc154320dec013b601748f1976
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-03-03 15:54:31 +0000

    fix the init.py file to export all classes

--- neuroseek/__init__.py
+++ neuroseek/__init__.py
@@ -1,4 +1,6 @@
 from neuroseek.vector import Vector
 from neuroseek.index import Index
+from neuroseek.hnsw_index import HNSWIndex
+from neuroseek.hnsw_node import HNSWNode
 
-__all__ = ['Vector', 'Index']
+__all__ = ['Vector', 'Index', 'HNSWIndex', 'HNSWNode']
