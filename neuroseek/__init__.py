5d5d6a1e5c1f03a7a44f1a9e1060a64fce207095
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-03-07 16:42:33 +0000

    fix: sync __version__ to 0.2.0

--- neuroseek/__init__.py
+++ neuroseek/__init__.py
@@ -11,7 +11,7 @@ from neuroseek.persistence.namespace_manager_persistence import save_namespace_m
 from neuroseek.ingestion.chunker import chunk_text
 from neuroseek.ingestion.ingestor import ingest_file, ingest_directory, SUPPORTED_EXTENSIONS
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     'Vector',
